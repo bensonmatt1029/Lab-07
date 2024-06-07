3e01a573fbbdd29295b75bb8296abedf8fa597a6
bensonmatt1029 <145501134+bensonmatt1029@users.noreply.github.com>
2024-06-06 20:55:49 -0600

    Add project files.

---
+++ Lab 07.cpp
@@ -0,0 +1,16 @@
+/**************************************************************************
+* Authors: 
+*  Matt Benson and Daniel Malasky
+* Summary:
+*  Physics for the Artillery Prototype
+**************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
