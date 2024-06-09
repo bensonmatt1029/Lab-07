77364899372baa477120b2aa25efaf3162a6675d
bensonmatt1029 <145501134+bensonmatt1029@users.noreply.github.com>
2024-06-08 19:13:46 -0600

    finished the physics!

--- Lab 07.cpp
+++ Lab 07.cpp
@@ -6,37 +6,32 @@
 **************************************************************************/
 
 #define _USE_MATH_DEFINES
-#include <math.h>   // for M_PI which is 3.14159
-
-
-#include "velocity.h"
-#include "acceleration.h"
-#include "position.h"
-#include <utility>
-#include <vector>
-#include <cmath>    // for sin, cos
-#include <iostream>
+#include <math.h>             // for M_PI 
+#include "velocity.h"         // for velocity
+#include <vector>             // for vector
+#include <cmath>              // for sin, cos
+#include <iostream>           // for iostream
 using namespace std;
 
-#define WEIGHT    46.7   // weight of projectile
-#define DIAMETER  154.89 // diameter of projectile
-#define VELOCITYM 827.0  // muzzle velocity
-#define TIME      0.01   // time interval
-#define ACC       43.867 // acceleration
-
+#define WEIGHT    46.7        // weight of projectile
+#define VELOCITYM 827.0       // muzzle velocity
+#define TIME      0.01        // time interval
+#define SURFACEAREA  0.018842 //in m^2 for drag
 
 /****************************************************
 * Table values
+* Altitude, Speed of Sound, Air Density, Mach values,
+* Drag Coefficients, Gravity Values
 ****************************************************/
 // Altitude values
-std::vector<int> altitudes =
+std::vector<double> altitudes =
 {
  0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000,
  10000, 15000, 20000, 25000, 30000, 40000, 50000, 60000, 70000, 80000
 };
 
 // Speed of Sound Values
-std::vector<int> speedOfSound =
+std::vector<double> speedOfSound =
 {
  340, 336, 332, 328, 324, 320, 316, 312, 308, 303,
  299, 295, 295, 295, 305, 324, 337, 319, 289, 269
@@ -72,6 +67,14 @@ std::vector<double> gravities =
  9.788, 9.785, 9.782, 9.779, 9.776, 9.761, 9.745, 9.730
 };
 
+/*********************************************************************
+* COMPUTE DRAG
+* drag coefficient, airDensity, magnitude of Velocity, surface area
+*********************************************************************/
+double computeDrag(double c, double p, double v, double a)
+{
+   return (0.5 * c * p * (v * v) * a);
+}
 
 /****************************************************
 * Linear Interpolation
@@ -80,13 +83,14 @@ std::vector<double> gravities =
 * x1,y1 = coordinates of second point
 * x,y   = coordinates of a point in the middle
 ****************************************************/
-double linearInterpolation(double x0, double y0, double x1, double y1, double x) 
+double linearInterpolation(double x0, double y0,
+   double x1, double y1, double x) 
 {
    return y0 + (y1 - y0) * ((x - x0) / (x1 - x0));
 }
 
 /****************************************************
-* ConvertToRadians
+* CONVERT TO RADAINS
 * Take an angle and convert it to radians
 ****************************************************/
 double convertToRadians(double degrees)
@@ -94,7 +98,7 @@ double convertToRadians(double degrees)
    return degrees / 360 * (2 * M_PI);
 }
 
-/***************************************************
+/********************************************************************
 * COMPUTE DISTANCE
 * Apply inertia to compute a new position using the distance equation.
 * The equation is:
@@ -106,7 +110,7 @@ double convertToRadians(double degrees)
 *     t : time, in seconds
 * OUTPUT
 *     s : new position, in meters
-**************************************************/
+*******************************************************************/
 double computeDistance(double initialS, double v, double a, double t)
 {
    double s;
@@ -114,129 +118,176 @@ double computeDistance(double initialS, double v, double a, double t)
    return s;
 }
 
-///******************************************************
-//* setGravity
-//* uses interpolation and values from the table
-//******************************************************/
-//int getIAltitudes(double altitude, int iAltitudes)
-//{
-//   
-//   
-//   //cout << "ALEVEL: " << aLevel << endl;
-//   if (altitude > altitudes[iAltitudes+1] )
-//   {
-//      iAltitudes++;
-//      
-//   }
-//   else if (altitude < altitudes[iAltitudes])
-//   {
-//      iAltitudes--;
-//     
-//   }
-//   return iAltitudes;
-//}
-
 /******************************************************
-* Get index of altitude in the altitude table
-* Returns the index of the closest altitude to the given altitude
+* GET TABLE INDEX
+* Get index of a given value in the given table
+* Returns the index of the closest values to the given value
 ******************************************************/
-int getAltitudeIndex(double altitude)
+int getTableIndex(double value, vector<double> values)
 {
    int index = 0;
-   while (index < altitudes.size() - 1 && altitude > altitudes[index + 1]) {
+   while (index < values.size() - 1 && value > values[index + 1]) 
+   {
       index++;
    }
    return index;
 }
 
-/******************************************************
+/****************************************************************
+* COMPUTE GRAVITY
 * Compute gravity at a given altitude
 * Uses linear interpolation to find gravity at the given altitude
-******************************************************/
+*****************************************************************/
 double computeGravity(double altitude)
 {
-   int index = getAltitudeIndex(altitude);
-   if (index == altitudes.size() - 1) {
+   int index = getTableIndex(altitude, altitudes);
+   if (index == altitudes.size() - 1) 
+   {
       return gravities[index];
    }
-   else {
-      double gravity = linearInterpolation(altitudes[index], gravities[index], altitudes[index + 1], gravities[index + 1], altitude);
+   else 
+   {
+      double gravity = linearInterpolation(altitudes[index],
+      gravities[index], altitudes[index + 1], gravities[index + 1], altitude);
       return gravity;
    }
 }
 
-void setGravity(double altitude, int iAltitude)
+/****************************************************************
+* COMPUTE SPEED OF SOUND
+* Uses linear interpolation to find speed of sound at the given altitude
+*****************************************************************/
+double computeSpeedOfSound(double altitude)
 {
-
+   int index = getTableIndex(altitude, altitudes);
+   if (index == altitudes.size() - 1) 
+   {
+      return speedOfSound[index];
+   }
+   else 
+   {
+      double soundSpeed = linearInterpolation(altitudes[index], 
+         speedOfSound[index], altitudes[index + 1], 
+         speedOfSound[index + 1], altitude);
+      return soundSpeed;
+   }
 }
 
-int main()
+/******************************************************
+* COMPUTE DRAG COEFFICIENT
+* Uses linear interpolation to find drag at the given mach
+******************************************************/
+double computeDragCo(double mach)
 {
-   Acceleration a;
+   int index = getTableIndex(mach, machNumbers);
+   if (index == machNumbers.size() - 1) 
+   {
+      return machNumbers[index];
+   }
+   else 
+   {
+      double dragCo = linearInterpolation(machNumbers[index], 
+         dragCoefficients[index], machNumbers[index + 1], 
+         dragCoefficients[index + 1], mach);
+      return dragCo;
+   }
+}
 
-   double angle = convertToRadians(75);
+/***************************************************************
+* COMPUTE AIR DENSITY
+* Uses linear interpolation to find gravity at the given altitude
+****************************************************************/
+double computeAirDensity(double altitude)
+{
+   int index = getTableIndex(altitude, altitudes);
+   if (index == densities.size() - 1) 
+   {
+      return densities[index];
+   }
+   else 
+   {
+      double airDensity = linearInterpolation(altitudes[index], 
+      densities[index],altitudes[index + 1], densities[index + 1], altitude);
+      return airDensity;
+   }
+}
+
+/*************************************************************
+* DISPLAY BULLET TRAVEL
+* Displays the distance and hang time of a 
+* M795 High Explosive 155mm artillery projectile
+* from the M777 howitzer
+******************************************************/
+void displayBulletTravel(double angleDegrees) 
+{
+   double angle = convertToRadians(angleDegrees);
    Velocity velocity(sin(angle) * VELOCITYM, cos(angle) * VELOCITYM);
-
-   Position position;
-   double degrees = 75;
-   double radians = convertToRadians(75);
-   double radius = DIAMETER / 2.0;
-   double surfaceA = M_PI * (radius * radius);
-
-   //cout << "What is the angle of the howitzer where 0 is up?";
-   //cin >> angle;
-
-   //cout << (linearInterpolation(0.2897, 1.900, 0.2297, 2.870, 2.4323));
 
    double distance = 0.0;
    double altitude = 0.0;
    double hangTime = 0.0;
+   double gravity;
+   double airDensity;
+   double dragCo;
+   double soundSpeed;
 
+   // second to last values
+   double distance0 = 0;
+   double altitude0 = 0;
 
-   double gravity = -9.807;
-   
-   // leves on the grav table
-   int iAltitudes = 0;
-   int iGravities = 0;
-
-   //for (int i = 0; i < 20; i++)
-   while (altitude >= 0.0)
-   {
-
-      //iAltitudes = getIAltitudes(altitude, iAltitudes);
-      //iGravities = iAltitudes;
-      
-
+   while (altitude >= 0.0) {
+      // Compute values 
       gravity = computeGravity(altitude);
-      cout << "GRAV: " << gravity << endl;
-      
+      airDensity = computeAirDensity(altitude);
+      soundSpeed = computeSpeedOfSound(altitude);
+      dragCo = computeDragCo(velocity.getSpeed() / soundSpeed);
 
-      velocity.addDY(-gravity * TIME);
+      // Calculate drag force
+      double totalVelocity = velocity.getSpeed();
+      double dragForce = computeDrag(dragCo, airDensity,
+         totalVelocity, SURFACEAREA);
+      double dragAcceleration = dragForce / WEIGHT;
 
+      // Calculate components of drag acceleration
+      double dragAccX = -dragAcceleration * (velocity.getDX() / totalVelocity);
+      double dragAccY = -dragAcceleration * (velocity.getDY() / totalVelocity);
+
+      // Update velocities with drag and gravity
+      velocity.addDX(dragAccX * TIME);
+      velocity.addDY((-gravity + dragAccY) * TIME);
+
+      // Update positions
       distance += computeDistance(0.0, velocity.getDX(), 0.0, TIME);
       altitude += computeDistance(0.0, velocity.getDY(), -gravity, TIME);
 
-      cout << "Distance: " << distance << " meters\n";
-      cout << "Altitude: " << altitude << " meters\n";
-      cout << "Velocity DY: " << velocity.getDY() << " m/s\n\n";
-      cout << "Hang Time: " << hangTime << "s\n\n";
+      // update hangTime
+      hangTime += TIME;
 
-      hangTime += 0.01;
       
+      // second to last distance, altitude for Ground Impact
+      if (altitude > 0)
+      {
+         distance0 = distance;
+         altitude0 = altitude;
+      }
    }
+   
+   // Ground Impact
+   distance = linearInterpolation(altitude0, distance0, altitude, distance, 0);
 
-
-
-
+   cout << "Distance: " << distance << " meters\t";
+   cout << "Hang Time: " << hangTime << "s\n\n";
 }
 
-
-// Ask about inertia
-
-// distance formula is inertia
-
-// gravity - use linear interpolation and a return a value from a table
-// drag - same but new table. drag force equatoin
-
-
-// include position.h, velocity.h, and angle.h
+/*************************************************************
+* MAIN
+* There can be only one!
+******************************************************/
+int main()
+{
+   // Test Cases
+   displayBulletTravel(0);
+   displayBulletTravel(30);
+   displayBulletTravel(60);
+   displayBulletTravel(-45);
+}
